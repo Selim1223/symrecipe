97c33a5a4bce2c02fe25d4b91c2c92eacdde1f48
Selim <zouache.selim57@gmail.com>
2022-09-14 13:41:30 +0200

    new commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,11 +4,13 @@ namespace App\DataFixtures;
 
 
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
-use App\Entity\Ingredient;
 use App\Entity\Recipe;
+use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -17,10 +19,13 @@ class AppFixtures extends Fixture
      */
      private Generator $faker;
 
+
      public function __construct()
      {
         $this->faker = Factory::create('fr_FR');
+        
      }
+  
 
     public function load(ObjectManager $manager): void
     {
@@ -51,6 +56,20 @@ class AppFixtures extends Fixture
             $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
            }
            $manager->persist($recipe);
+        }
+
+
+        //Users
+        for ($i=0; $i < 10; $i++) { 
+           $user = new User();
+           $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0,1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+
+            
+            $manager->persist($user);
         }
 
         $manager->flush();
